7018f65832154d61edb56d176e6f47ddd7d4e021
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2023-02-22 21:17:39 -0500

    Step 1 Complete

--- lab-insta-parse/Models/User.swift
+++ lab-insta-parse/Models/User.swift
@@ -8,7 +8,26 @@
 import Foundation
 
 // TODO: Pt 1 - Import Parse Swift
+import ParseSwift
 
 
 // TODO: Pt 1 - Create Parse User model
 // https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/3%20-%20User%20-%20Sign%20Up.xcplaygroundpage/Contents.swift#L16
+struct User: ParseUser {
+    /// Required by `ParseObject`
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+    
+    /// Required by `ParseUser`
+    var username: String?
+    var email: String?
+    var emailVerified: Bool?
+    var password: String?
+    var authData: [String : [String : String]?]?
+    
+    // Your custom keys
+    // var customKey: String?
+}
