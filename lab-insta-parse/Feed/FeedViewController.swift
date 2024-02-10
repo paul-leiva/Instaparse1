ba13e9c9c2f2da03c5d5a055e496573388738327
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2024-02-10 15:54:27 -0500

    Step 3 - Parse User Log Out

--- lab-insta-parse/Feed/FeedViewController.swift
+++ lab-insta-parse/Feed/FeedViewController.swift
@@ -59,6 +59,22 @@ class FeedViewController: UIViewController {
         }
 
     }
+    
+    @IBAction func onLogOutTapped(_ sender: UIBarButtonItem) {
+        showConfirmLogoutAlert()
+    }
+    
+    private func showConfirmLogoutAlert() {
+        let alertController = UIAlertController(title: "Log out of your account?", message: nil, preferredStyle: .alert)
+        let logOutAction = UIAlertAction(title: "Log out", style: .destructive) { _ in
+            NotificationCenter.default.post(name: Notification.Name("logout"), object: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(logOutAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+    
 
     private func showAlert(description: String? = nil) {
         let alertController = UIAlertController(title: "Oops...", message: "\(description ?? "Please try again...")", preferredStyle: .alert)
